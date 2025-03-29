d28aaabd27c4d9b138bc111e9cd4d49b685456a5
RS.Fan <2575205293@qq.com>
2025-03-29 21:40:38 +0800

    Add files via upload

---
+++ joe/main.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <cstdio>
+#include <graphics.h>
+#include <easyx.h>
+#include <algorithm>
+#include <time.h>
+#include <queue>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <cstdio>
+#include "game.h"
+#include "button.h"
+using namespace std;
+
+
+
+
+
+
+//#define default_height textheight("һ")
+//#define four_text_w textwidth("һһһһ")
+//#define two_text_w textwidth("һһһһ")
+//
+
+
+
+#ifdef UNICODE
+#undef UNICODE
+#endif
+
+void controller();
+
+void menu_view();
+
+void setting_view();
+
+void help_view();
+
+void about_view();
+
+
+
+
+
+
